843ba26f8d47e269a465f78d13e184d3c8e54134
DanielSnd <daniel.snd.anim@gmail.com>
2024-03-01 16:39:48 -0400

    Sprite3d automatically set texture_albedo on override material

--- scene/3d/sprite_3d.cpp
+++ scene/3d/sprite_3d.cpp
@@ -277,6 +277,11 @@ void SpriteBase3D::draw_texture_rect(Ref<Texture2D> p_texture, Rect2 p_dst_rect,
 	}
 	if (last_texture != p_texture->get_rid()) {
 		RS::get_singleton()->material_set_param(get_material(), "texture_albedo", p_texture->get_rid());
+		if (!get_material_override().is_null() && get_material_override().is_valid()) {
+			if (Material* overridemat = get_material_override().ptr(); overridemat != nullptr) {
+				RS::get_singleton()->material_set_param(overridemat->get_rid(),"texture_albedo",p_texture->get_rid());
+			}
+		}
 		last_texture = p_texture->get_rid();
 	}
 	if (get_alpha_cut_mode() == ALPHA_CUT_DISABLED) {
