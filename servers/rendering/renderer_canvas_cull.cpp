c3ad5d17b28289f48c76b0cbe6332b5b81bd055c
Daniel Snd <daniel@bittentoast.com>
2024-04-10 20:21:28 -0300

    Reimplemented missing methods for set_z_index on canvas occluder

--- servers/rendering/renderer_canvas_cull.cpp
+++ servers/rendering/renderer_canvas_cull.cpp
@@ -2019,6 +2019,13 @@ void RendererCanvasCull::canvas_light_occluder_set_z_index(RID p_occluder, int p
 	occluder->z_index = p_z_index;
 }
 
+void RendererCanvasCull::canvas_light_occluder_set_z_index(RID p_occluder, int p_z_index) {
+	RendererCanvasRender::LightOccluderInstance *occluder = canvas_light_occluder_owner.get_or_null(p_occluder);
+	ERR_FAIL_NULL(occluder);
+
+	occluder->z_index = p_z_index;
+}
+
 RID RendererCanvasCull::canvas_occluder_polygon_allocate() {
 	return canvas_light_occluder_polygon_owner.allocate_rid();
 }
