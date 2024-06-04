7641523b8803e0813da4f8fc94b7934794582843
Ed Lu <ed@megacrit.com>
2024-06-04 16:12:38 -0700

    Added Breakpad crash dump generation
    Fixed compiling on Windows
    
    Make crash dump printing work better on Windows
    
    Tweaked Breakpad build
    
    Fixed Breakpad build for Godot 4
    
    Switched to WINDOWS_ENABLED
    
    Removed some non buildable files listed in the files to build
    
    Ran clang format
    
    Fixed the other formatting issues detected by CI
    
    Removed a comment and added clarifying comment on crash dump message
    
    as to why it is printed twice on Windows
    
    Make an ugly string conversion to make Windows build work
    
    Tweaked the build configuration and formatted again
    
    removed lss
    
    Add lss properly
    
    Reinitialize breakpad after mono initialization on Linux
    
    otherwise the breakpad signal handlers are not active
    
    Disable Windows crash handler on destruction similarly to Linux
    
    Renamed breakpad_enabled to use_breakpad
    
    Forgot to wrap one piece of code inside ifdef USE_BREAKPAD
    
    Updated copyright years in the added files
    
    Fix register types for breakpad
    
    Fixed dir access
    
    Removed musl and elf_reader
    
    which was the only thing seemingly depending on it
    
    Updated header guards
    
    Removed the memdelete call

--- platform/windows/crash_handler_windows_seh.cpp
+++ platform/windows/crash_handler_windows_seh.cpp
@@ -37,6 +37,10 @@
 #include "main/main.h"
 #include "servers/display_server.h"
 
+#ifdef USE_BREAKPAD
+#include "modules/breakpad/breakpad.h"
+#endif
+
 #ifdef CRASH_HANDLER_EXCEPTION
 
 // Backtrace code based on: https://stackoverflow.com/questions/6205981/windows-c-stack-trace-from-a-running-app
@@ -117,6 +121,10 @@ public:
 };
 
 DWORD CrashHandlerException(EXCEPTION_POINTERS *ep) {
+#ifdef USE_BREAKPAD
+	breakpad_handle_exception_pointers(static_cast<void *>(ep));
+#endif
+
 	HANDLE process = GetCurrentProcess();
 	HANDLE hThread = GetCurrentThread();
 	DWORD offset_from_symbol = 0;
@@ -266,6 +274,7 @@ CrashHandler::CrashHandler() {
 }
 
 CrashHandler::~CrashHandler() {
+	disable();
 }
 
 void CrashHandler::disable() {
@@ -273,8 +282,21 @@ void CrashHandler::disable() {
 		return;
 	}
 
+#ifdef USE_BREAKPAD
+	disable_breakpad();
+#endif
+
 	disabled = true;
 }
 
 void CrashHandler::initialize() {
+#ifdef CRASH_HANDLER_EXCEPTION
+
+#ifdef USE_BREAKPAD
+	initialize_breakpad(false);
+#endif
+
+#elif defined(USE_BREAKPAD)
+	initialize_breakpad(true);
+#endif
 }
